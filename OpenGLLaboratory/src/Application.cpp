92b237b18423b785eca9267e9be49061a7185461
Sravan Karuturi <Sravan Karuturi@Home>
2018-08-13 13:22:57 -0400

    Inital Commit

---
+++ OpenGLLaboratory/src/Application.cpp
@@ -0,0 +1,54 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main(int _argc, char* _argv[])
+{
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	GLFWwindow * window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Load Glad */
+	if (GLEW_OK != glewInit()) {
+		std::cout << "Failed to Init Glew" << std::endl;
+		return -1;
+	}
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+
+}
