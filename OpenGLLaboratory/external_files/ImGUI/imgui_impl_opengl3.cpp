f8a32d8fbc9e29adcd5229976c44fab240f789a7
Sravan Karuturi <Sravan Karuturi@Home>
2018-08-14 19:00:42 -0400

    Fixed a linking error.

--- OpenGLLaboratory/external_files/ImGUI/imgui_impl_opengl3.cpp
+++ OpenGLLaboratory/external_files/ImGUI/imgui_impl_opengl3.cpp
@@ -27,7 +27,11 @@
 
 #include "imgui.h"
 #include "imgui_impl_opengl3.h"
-#include <GL/glew.h>    // This example is using gl3w to access OpenGL functions. You may freely use any other OpenGL loader such as: glew, glad, glLoadGen, etc.
+#ifndef GLEW_STATIC
+#define GLEW_STATIC
+#include <GL/glew.h>
+#endif
+// This example is using gl3w to access OpenGL functions. You may freely use any other OpenGL loader such as: glew, glad, glLoadGen, etc.
 //#include <glew.h>
 
 // OpenGL Data
