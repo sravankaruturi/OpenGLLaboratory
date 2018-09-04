8d30d61dc5e06a820d18ad04eae073375583126a
Sravan <knight2356@gmail.com>
2018-09-04 15:29:01 -0400

    WIP: Skeletal Animation
    Still has a lot way to go.

--- OpenGLLaboratory/src/Concepts/ConceptSkeletalMesh.cpp
+++ OpenGLLaboratory/src/Concepts/ConceptSkeletalMesh.cpp
@@ -17,6 +17,17 @@
 namespace olab {
 	namespace concepts {
 
+		// This just copies. This function doesn't transpose matrices.
+		void convert_aimatrix_to_glm(glm::mat4& _glmMat4, const aiMatrix4x4& _aiMatrix) {
+
+			for (auto i = 0; i < 4; i++) {
+				for (auto j = 0; j < 4; j++) {
+					_glmMat4[i][j] = _aiMatrix[i][j];
+				}
+			}
+
+		}
+
 		ConceptSkeletalMesh::ConceptSkeletalMesh()
 			: skinningShader("Assets/Shaders/Concept_mvp.shader"),
 			position(0.0f, 0.0f, 0.0f),
@@ -37,13 +48,13 @@ namespace olab {
 
 			projectionMatrix = glm::perspective(glm::radians(45.0f), 16.0f / 9.0f, 0.f, 100.f);
 
-#if 0
+#if 1
 			std::string path = "Z:/IGMProfile/Desktop/Projects/OpenGLLaboratory/OpenGLLaboratory/Assets/Models/boblamp/boblampclean.md5mesh";
 #else
 			std::string path = "C:/dev/OpenGLLaboratory/OpenGLLaboratory/Assets/Models/boblamp/boblampclean.md5mesh";
 #endif
 
-			ConceptModelLoading::LoadMesh(path);
+			this->LoadMesh(path);
 
 			this->position = glm::vec3(0, 0, 0);
 			this->rotation = glm::vec3(90, 180, 180);
@@ -108,6 +119,157 @@ namespace olab {
 			ImGui::Text("Projection Matrix");
 			ImGui::SliderFloat("Field Of View", &fieldOfView, 0.5f, 90.0f, "%.3f", 2.0f);
 
+		}
+
+		void ConceptSkeletalMesh::LoadMesh(std::string _path)
+		{
+			Assimp::Importer import;
+			const aiScene *scene = import.ReadFile(_path, aiProcess_Triangulate | aiProcess_FlipUVs);
+
+			if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+			{
+				std::cout << "ERROR::ASSIMP::" << import.GetErrorString() << std::endl;
+				return;
+			}
+			std::string directory = _path.substr(0, _path.find_last_of('/'));
+
+			this->ProcessNode(scene->mRootNode, scene);
+
+			Shader * shader = new Shader("Assets/Shaders/Concept_mvp.shader");
+
+			shader->use();
+			shader->setMat4("u_ModelMatrix", glm::scale(glm::mat4(1.0f), glm::vec3(0.2f, 0.2f, 0.2f)));
+			shader->setMat4("u_ViewMatrix", glm::lookAt(glm::vec3(0.0f, 0.0f, 10.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f)));
+			shader->setMat4("u_ProjectionMatrix", glm::perspective(glm::radians(45.0f), 16.0f / 9.0f, 0.1f, 100.0f));
+
+			for (auto& it : meshes)
+			{
+				it.shader = shader;
+			}
+
+		}
+
+		void ConceptSkeletalMesh::ProcessNode(aiNode * _node, const aiScene * _scene)
+		{
+			// process all the node's meshes (if any)
+			for (unsigned int i = 0; i < _node->mNumMeshes; i++)
+			{
+				aiMesh *mesh = _scene->mMeshes[_node->mMeshes[i]];
+				meshes.push_back(this->ProcessMesh(mesh, _scene));
+			}
+			// then do the same for each of its children
+			for (unsigned int i = 0; i < _node->mNumChildren; i++)
+			{
+				this->ProcessNode(_node->mChildren[i], _scene);
+			}
+
+		}
+
+		Mesh ConceptSkeletalMesh::ProcessMesh(aiMesh * _mesh, const aiScene * _scene)
+		{
+			Mesh return_mesh;
+
+			std::vector<float> vertices;
+			std::vector<unsigned int> indices;
+
+			for (unsigned int i = 0; i < _mesh->mNumVertices; i++)
+			{
+				float tex_x = 0.0f;
+				float tex_y = 0.0f;
+
+				if (_mesh->mTextureCoords[0])
+				{
+					tex_x = _mesh->mTextureCoords[0][i].x;
+					tex_y = _mesh->mTextureCoords[0][i].y;
+				}
+
+				vertices.push_back(_mesh->mVertices[i].x);
+				vertices.push_back(_mesh->mVertices[i].y);
+				vertices.push_back(_mesh->mVertices[i].z);
+
+				//vertices.push_back(_mesh->mNormals[i].x);
+				//vertices.push_back(_mesh->mNormals[i].y);
+				//vertices.push_back(_mesh->mNormals[i].z);
+
+				vertices.push_back(tex_x);
+				vertices.push_back(tex_y);
+			}
+
+			// A Bone or bones for each mesh.
+			for (auto i = 0; i < _mesh->mNumBones; i++) {
+
+				auto bone_index = 0;
+				std::string bone_name(_mesh->mBones[i]->mName.data);
+
+				// We could not find the bone in our map. Add it and add it to the Bone Info Data
+				if (boneMapping.find(bone_name) == boneMapping.end()) {
+
+					// TODO: If some thing doesn't work, maybe store this in a separate variable like he does.
+					bone_index = boneMapping.size();
+
+					BoneInfo bi;
+					// Since the GLM Matrices are Column Major, 
+					convert_aimatrix_to_glm(bi.boneOffset, _mesh->mBones[i]->mOffsetMatrix);
+					
+					boneInfoData.push_back(bi);
+
+					boneMapping[bone_name] = bone_index;
+
+				}
+				else {
+					bone_index = boneMapping[bone_name];
+				}
+
+				for (auto j = 0; j < _mesh->mBones[i]->mNumWeights; j++) {
+					// Since we separate each mesh, the base vertex would be 0. ??
+					auto vertex_id = 0 + _mesh->mBones[i]->mWeights[j].mVertexId;
+					auto weight = _mesh->mBones[i]->mWeights[j].mWeight;
+					this->vertexBoneData[vertex_id].AddBoneData(bone_index, weight);
+				}
+
+			}
+
+			// process indices
+			for (unsigned int i = 0; i < _mesh->mNumFaces; i++)
+			{
+				aiFace face = _mesh->mFaces[i];
+				// retrieve all indices of the face and store them in the indices vector
+				for (unsigned int j = 0; j < face.mNumIndices; j++)
+					indices.push_back(face.mIndices[j]);
+			}
+
+			aiMaterial* material = _scene->mMaterials[_mesh->mMaterialIndex];
+			const std::vector<Texture *> diffuseMaps = LoadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
+
+			const unsigned int __temp = sizeof(float) * vertices.size();
+
+			VertexBuffer * vb = new VertexBuffer(&vertices[0], __temp);
+			VertexBufferLayout vbl;
+			vbl.Push<float>(3);
+			//vbl.Push<float>(3);
+			vbl.Push<float>(2);
+
+			return_mesh.vb = vb;
+			return_mesh.va = new VertexArray();
+			return_mesh.va->AddBuffer(*vb, vbl);
+			return_mesh.ib = new olab::IndexBuffer(&indices[0], indices.size());
+			return_mesh.textures = diffuseMaps;
+
+			return return_mesh;
+		}
+
+		void VertexBoneData::AddBoneData(unsigned int _boneID, float _weight)
+		{
+			for (auto i = 0; i < sizeof(Ids)/sizeof(unsigned int); i++) {
+				if (weights[i] == 0.0) {
+					Ids[i] = _boneID;
+					weights[i] = _weight;
+					return;
+				}
+			}
+
+			// should never get here - more bones than we have space for
+			assert(0);
 		}
 
 	}
--- OpenGLLaboratory/src/Concepts/ConceptSkeletalMesh.h
+++ OpenGLLaboratory/src/Concepts/ConceptSkeletalMesh.h
@@ -5,9 +5,36 @@
 #include "../VertexBufferLayout.h"
 #include <assimp/material.h>
 
+#include <map>
+
+#define NUM_BONES_PER_VERTEX	4
+
 namespace olab {
 
 	namespace concepts {
+
+		struct BoneInfo {
+			glm::mat4 boneOffset;
+			glm::mat4 finalTransformation;
+		};
+
+		// The Bone Data for a specific vertex. It would ass the Indices of the Bones from the Bone Info Structure, and the Weight of each bone.
+		struct VertexBoneData {
+			unsigned int Ids[NUM_BONES_PER_VERTEX];
+			float weights[NUM_BONES_PER_VERTEX];
+
+			VertexBoneData() {
+				Reset();
+			}
+
+			void Reset() {
+				memset(Ids, 0, sizeof(Ids));
+				memset(weights, 0, sizeof(weights));
+			}
+
+			void AddBoneData(unsigned int _boneID, float _weight);
+
+		};
 
 		class ConceptSkeletalMesh : public ConceptModelLoading {
 
@@ -27,6 +54,11 @@ namespace olab {
 			float fieldOfView;
 			glm::mat4 projectionMatrix;
 
+			std::vector<VertexBoneData> vertexBoneData;
+			std::map<std::string, unsigned int> boneMapping; // Maps a bone to its index in BoneInfo
+
+			std::vector<BoneInfo> boneInfoData;
+
 		public:
 
 			ConceptSkeletalMesh();
@@ -35,6 +67,12 @@ namespace olab {
 			void OnUpdate(float _deltaTime);
 			void OnRender(const Renderer& _renderer);
 			void OnImGuiRender();
+
+			void LoadMesh(std::string _path);
+			void ProcessNode(aiNode * _node, const aiScene * _scene);
+
+			// This should be a different type of mesh since we do not have the structure ready for the bones in the previous mesh data.
+			Mesh ProcessMesh(aiMesh *_mesh, const aiScene *_scene);
 
 		};
 
